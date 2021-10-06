0cf2af63c42aa986acf412187b5ced60bf219efa
ankushmalode <you@example.com>
2021-10-06 16:28:00 +0530

    Calculating mulptipal company wage UC8

--- src/main/java/com/bridgelabz/EmployeeWage.java
+++ src/main/java/com/bridgelabz/EmployeeWage.java
@@ -2,47 +2,50 @@ package com.bridgelabz;
 
 import java.util.*;
 
+/**
+ * Employee Wage Program By Using ops Concept day-9
+ *
+ * @author : Ankush Malode
+ * @version : 16.0
+ * @since : 02/10/2021
+ */
 public class EmployeeWage {
+    public static final int isFullTime = 1;
+    public static final int isPartTime = 2;
+
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
-        int totalHr = 0, wagePerHr = 20, fullDayHr = 8, partTimeHr = 4, dayPerMonth = 20;
-        /*
-        calculating wage for a month
-         */
-        int i=0;
-     for (i=1;i<=dayPerMonth && totalHr<100;i++){
-         Random num = new Random();
-         int Attendance = num.nextInt(3);
-        /*Checking employee is
-        present or absent
-         */
-         if (totalHr<=100){
-             switch (Attendance) {
-                 case 1:
-//                    System.out.print("Employee is Present");
-                     totalHr += fullDayHr;
-                     if (totalHr>100)
-                         totalHr-=fullDayHr;
-                     break;
-                 case 2:
-//                    System.out.print("Employee is Present but only half-day");
-                     totalHr += partTimeHr;
-                     if (totalHr>100)
-                         totalHr-=partTimeHr;
-                     break;
-                 default:
-//                    System.out.print("Employee is Absent");
-                     totalHr += 0;
-//                 dayPerMonth++;
-                     i--;
-                     break;
-             }
-             System.out.println(i);
-//         System.out.println(dayPerMonth);
-             System.out.println("\nMonthly Employee Wage is: " + totalHr);
-         }
-         else
-             break;
-     }
-     }
+        wageCalculation("cognizant", 500, 20,100);
+        wageCalculation("capgemini", 800, 10,90);
+    }
+
+    public static void wageCalculation(String company, int empRatePerHr, int workingDayPerMonth, int maxPerHrMonth) {
+        int totalHr = 0, totalWorkingDay=0, fullDayHr = 8, partTimeHr = 4, totalEmpWage;        //calculating wage for a month
+        while (totalHr<=maxPerHrMonth && totalWorkingDay<workingDayPerMonth){
+            totalWorkingDay++;
+            Random num = new Random();
+            int Attendance = num.nextInt(3);//        Checking employee is present or absent
+                switch (Attendance) {
+                    case isFullTime:
+                        totalHr += fullDayHr;
+                        if (totalHr > 100)
+                            totalHr -= fullDayHr;
+                        break;
+                    case isPartTime:
+                        totalHr += partTimeHr;
+                        if (totalHr > 100)
+                            totalHr -= partTimeHr;
+                        break;
+                    default:
+                        totalHr += 0;
+                        totalWorkingDay--;/*if employee is absent the then
+                         * day also not to be count.
+                         */
+                        break;
+                }
+            }
+        System.out.println("\nMonthly Employee Wage is: " + totalHr);
+        totalEmpWage=totalHr*empRatePerHr;
+        System.out.println("In "+company+" per month employee wage is "+totalEmpWage);
+    }
 }
